5e0b592c3fe63050d1c317d2a568720b34d9b9fc
jagadeesh <meduri.jagadeesh@hcl.com>
2020-02-14 20:37:29 +0530

    adduser

---
+++ tests/addUser.js
@@ -0,0 +1,44 @@
+module.exports = {
+    'Login HomePage' : function (browser) {
+      browser
+        .url("http://ec2-35-166-133-120.us-west-2.compute.amazonaws.com/admin/login")
+        .waitForElementVisible('.header')
+        .assert.titleContains('Juliette - Login')
+        .setValue('#email', 'admin@juliettelaundry.com')
+        .setValue('input[id="pwd"]','juliette')
+        .click('input[type="button"]')
+        .assert.titleContains('Juliette - App Users')
+        .waitForElementPresent('.rmm-toggled-title')
+    },
+
+    'Add User' : function (browser) {
+        browser
+        .useXpath()
+        .click('//a[contains(text(),"Add New")]')
+        .useCss()
+        .setValue('#invite_id','TYU')
+        .setValue('#apartment','104')
+        //.setValue('#acode','123')
+        .setValue('#fname','scott')
+        .setValue('#lname','William')
+        .setValue('#email','mail1@mailinator.com')
+        .setValue('#phone1','123')
+        .setValue('#phone2','423')
+        .setValue('#phone1','1232')
+        .setValue('#zip','107325')
+        .setValue('#password','cool@123')
+        .setValue('#cpwd','cool@123')
+        .setValue('#notes','Testing')
+        //to do verify user is created
+        .click('input[type="submit"]')
+
+    },
+
+    'Logout' : function (browser) {
+        browser
+        .useXpath()
+        .click('//a[contains(text(),"logout")]')
+        .useCss()
+        .end()
+      },
+  };
