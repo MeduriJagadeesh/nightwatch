1326eeb8f1f2f96b1013a2ba1fb24cb89e03a6b8
Madhivanan <mbhojan@thapovan-inc.com>
2020-02-19 08:35:04 +0530

    changes in launch.js

--- tests/launch.js
+++ tests/launch.js
@@ -1,10 +1,9 @@
 module.exports = {
-    '@tags':['sanity'],
     'LogIn Page: verify Title and LogIn' : function (browser) {
-      var loginPage =browser.page.LoginPage();  
+      let loginPage =browser.page.LoginPage();  
       loginPage
         .navigate()
-        .verifyHeaderLoaded()
+        .verifyLoginPageLoaded()
         .performLogin('ravi@thapovan-inc.com','KHS}k/b|7Wfh2Bp')
       browser
         .assert.titleContains('Juliette - App Users')
